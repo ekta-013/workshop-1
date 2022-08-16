9f6d0ca93a467b6897311590a69137d2a144c7c1
ekta <ektamo0123@gmail.com>
2022-08-16 19:11:42 +0530

    Added : First commit

---
+++ com/bridgelabs/ticTacToe.java
@@ -0,0 +1,14 @@
+package com.bridgelabs;
+
+public class ticTacToe {
+
+	public static void main(String[] args) {
+		char[][] grid = new char[3][3];		
+		for(int i=0;i<3;i++) {
+			for(int j=0;j<3;j++)
+				grid[i][j] = ' ';						
+				System.out.println(i+ ":");
+				System.out.println(J+ ":");
+		}
+	}
+}
